5d129f49ab6ef590a4ebb95b0e6c0450deea4442
jeremy-abelard <jeremy97426@gmail.com>
2025-02-24 14:12:45 +0400

    first commit

---
+++ src/main/java/com/trackmyspend/trackmyspend/TrackmyspendApplication.java
@@ -0,0 +1,13 @@
+package com.trackmyspend.trackmyspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmyspendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmyspendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackmyspend/trackmyspend/TrackmyspendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmyspend.trackmyspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmyspendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
